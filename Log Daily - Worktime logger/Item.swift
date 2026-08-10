ae72f4ebfd9745f0e3c0d4bcac7400c74725189e
Riku Kuisma <130903460+Swifterhtmler@users.noreply.github.com>
2026-08-10 20:30:18 +0300

    first commit

---
+++ Log Daily - Worktime logger/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Log Daily - Worktime logger
+//
+//  Created by Riku Kuisma on 10.8.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Log-Daily---Worktime-Logger
