02928e766c99b57f983ab858ec31a6940718d2d5
Riku Kuisma <130903460+Swifterhtmler@users.noreply.github.com>
2026-08-11 20:48:16 +0300

    added new boilerplate

--- Log Daily - Worktime logger/ContentView.swift
+++ Log Daily - Worktime logger/ContentView.swift
@@ -14,33 +14,42 @@ struct ContentView: View {
     
     var body: some View {
         
-        List {
-            ForEach(items) { item in
-                Text(item.timestamp.description)
+        VStack(alignment: .leading) {
+            
+            VStack(alignment: .center) {
+                Button(action: addItem) {
+                    Label("Saavuin", systemImage: "plus")
+                    .padding(.top,320)
+                }
+            }.frame(maxWidth: .infinity)
+            
+            List {
+                ForEach(items) { item in
+                    Text(item.timestamp.description)
+                }
             }
         }
-        
     }
     
+    private func addItem() {
+        withAnimation {
+            //   let newItem = Item(timestamp: Date())
+            //   modelContext.insert(newItem)
+        }
+    }
+    //
+    //    private func deleteItems(offsets: IndexSet) {
+    //        withAnimation {
+    //            for index in offsets {
+    //                modelContext.delete(items[index])
+    //            }
+    //        }
+    //    }
+    //}
 }
+    
+    #Preview {
+        ContentView()
+        //  .modelContainer(for: Item.self, inMemory: true)
+    }
 
-//    private func addItem() {
-//        withAnimation {
-//            let newItem = Item(timestamp: Date())
-//            modelContext.insert(newItem)
-//        }
-//    }
-//
-//    private func deleteItems(offsets: IndexSet) {
-//        withAnimation {
-//            for index in offsets {
-//                modelContext.delete(items[index])
-//            }
-//        }
-//    }
-//}
-
-#Preview {
-    ContentView()
-      //  .modelContainer(for: Item.self, inMemory: true)
-}
