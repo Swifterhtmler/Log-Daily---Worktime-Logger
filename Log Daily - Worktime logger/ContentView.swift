619bcdbd453cd815411d9abb2eeb112274f33e08
Riku Kuisma <130903460+Swifterhtmler@users.noreply.github.com>
2026-08-13 21:19:28 +0300

    modified the button style and added new logView

--- Log Daily - Worktime logger/ContentView.swift
+++ Log Daily - Worktime logger/ContentView.swift
@@ -10,33 +10,76 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    @Query var items: [Item]
+    @State private var scale = 1.0
     
     var body: some View {
         
-        VStack(alignment: .leading) {
+        
+        NavigationView {
             
-            VStack(alignment: .center) {
-                Button(action: addItem) {
-                    Label("Saavuin", systemImage: "plus")
-                    .padding(.top,320)
-                }
-            }.frame(maxWidth: .infinity)
-            
-            List {
-                ForEach(items) { item in
-                    Text(item.timestamp.description)
-                        .font(.caption)
-                        .fontWeight(.heavy)
-                }
+            VStack(alignment: .leading) {
+                VStack(alignment: .center) {
+                    NavigationLink(destination: LogView().navigationBarBackButtonHidden(true), label: {
+                        Button(action: addItem) {
+                            Text("Saavuin")
+                                .padding(50)
+                                .font(.title2)
+                                .fontWeight(.bold)
+                            
+                        }
+                        .allowsHitTesting(false)
+                        .foregroundStyle(Color.black)
+                        .background(Color.gray)
+                        .cornerRadius(15)
+                        .clipShape(.circle)
+                        .scaleEffect(scale)
+                        .animation(.linear(duration: 1), value: scale)
+                        .padding(.top,10)
+                    })
+                }.frame(maxWidth: .infinity)
+                
+//                List {
+//                    ForEach(items) { item in
+//                        Text(item.timestamp.description)
+//                            .font(.caption)
+//                            .fontWeight(.heavy)
+//                    }
+//                }
+                
             }
+            
+            
+        }
+      
+        
+        
+    // the view ends here
+    }
+
+    
+    struct LogView: View {
+        @Environment(\.modelContext) private var modelContext
+        @Query private var items: [Item]
+        var body: some View {
+          //  Text("this is log view navigated to");
+                            List {
+                                ForEach(items) { item in
+                                    Text(item.timestamp.description)
+                                        .font(.caption)
+                                        .fontWeight(.heavy)
+                                }
+                            }
         }
     }
+    
+    
     
     private func addItem() {
         withAnimation {
                let newItem = Item(timestamp: Date())
                 modelContext.insert(newItem)
+                scale += 0.2
         }
     }
     //
